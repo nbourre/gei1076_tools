2946e4318bc717ae3109543175f0c3bfe867a6eb
Nicolas Bourré <nbourrereg@hotmail.com>
2020-10-14 11:44:41 -0400

    Test project added

---
+++ tools_tests/ToolsTests.cs
@@ -0,0 +1,20 @@
+﻿using nick_wpf_tools.ViewModels;
+using System;
+using Xunit;
+
+namespace tools_tests
+{
+    
+    public class ToolsTests
+    {
+        [Fact]
+        public void ClosePort_MustReturnFalseIfNotOpen()
+        {            
+            SerialPortViewModel sp = new SerialPortViewModel();
+
+            var actual = sp.Close();
+
+            Assert.False(actual);
+        }
+    }
+}
